9f28def95ee57ca7d05cf034a80e2f0860e3134c
Francesca Falci <francescafacia@gmail.com>
2023-04-26 15:52:19 -0300

    listaDos funcionando (sin interfaz)

--- listaDos.c
+++ listaDos.c
@@ -12,27 +12,44 @@ struct Tarea
 
 struct Nodo
 {
-    Tarea *T;
+    Tarea T;
     struct Nodo *siguiente;
 };
 
 typedef struct Nodo *Lista;
 
 Lista crearLista();
-Tarea *crearTarea(int id, int duracion, char descripcion[]);
-struct Nodo *crearNodo(Tarea *t);
-Lista insertarNodo(Lista *L , struct Nodo * nodoAInsertar);
+Tarea crearTarea(int id, int duracion, char descripcion[]);
+struct Nodo *crearNodo(Tarea t);
+struct Nodo *quitarNodo(Lista *L, int id);
+void insertarNodo(Lista *L , struct Nodo * nodoAInsertar);
 void mostrarTarea(Tarea t) ;
 void mostrarLista(Lista L);
 
 int main(){
 
-    Lista listaPendientes = crearLista();
-    Tarea *uno = crearTarea(1, 2, "uno");
-    // mostrarTarea(*uno);
-    listaPendientes = insertarNodo(&listaPendientes, crearNodo(uno));
+    Lista listaPendientes, listaRealizadas;
+    listaPendientes = crearLista();
+    listaRealizadas = crearLista();
+    struct nodo *nodoQuitado;
 
+    Tarea uno = crearTarea(1, 2, "uno");
+    Tarea dos = crearTarea(2, 2, "dos");
+    
+    // mostrarTarea(uno);
+    insertarNodo(&listaPendientes, crearNodo(uno));
+    insertarNodo(&listaPendientes, crearNodo(dos));
+    insertarNodo(&listaPendientes, crearNodo(crearTarea(3, 2, "tres")));
+    
+    nodoQuitado = quitarNodo(&listaPendientes, 2);
+    insertarNodo(&listaRealizadas, nodoQuitado);
+    puts("----PendienteS---");
     mostrarLista(listaPendientes);
+    puts("----Realizadas---");
+    mostrarLista(listaRealizadas);
+
+
+
 
     return 0;
 }
@@ -41,35 +58,57 @@ Lista crearLista(){
     return NULL;
 }
 
-Tarea *crearTarea(int id, int duracion, char descripcion[]){
-    Tarea *nuevaTarea = (Tarea *)malloc(sizeof(Tarea));
-    nuevaTarea->TareaID = id;
-    nuevaTarea->Duracion = duracion;
-    nuevaTarea->Descripcion = (char *)malloc((strlen(descripcion) + 1) * sizeof(char));
-    strcpy(nuevaTarea->Descripcion, descripcion);
+Tarea crearTarea(int id, int duracion, char descripcion[]){
+    Tarea nuevaTarea; //(Tarea *)malloc(sizeof(Tarea));
+    nuevaTarea.TareaID = id;
+    nuevaTarea.Duracion = duracion;
+    nuevaTarea.Descripcion = (char *)malloc((strlen(descripcion) + 1) * sizeof(char));
+    strcpy(nuevaTarea.Descripcion, descripcion);
     return nuevaTarea;
 }
 
-struct Nodo *crearNodo(Tarea *t){
+struct Nodo *crearNodo(Tarea t){
     struct Nodo *nuevoNodo = (struct Nodo*)malloc(sizeof(struct Nodo));
     nuevoNodo->T = t;
     nuevoNodo->siguiente = NULL;
 }
 
-Lista insertarNodo(Lista *L, struct Nodo *nodoAInsertar){
+struct Nodo *quitarNodo(Lista *L, int id){
+    Lista aux = *L;
+    Lista auxAnterior = *L;
+
+    while (aux != NULL && aux->T.TareaID != id)
+    {
+        auxAnterior = aux;
+        aux = aux->siguiente;
+    }
+
+    if(aux){
+        if(aux == *L){
+            *L = aux->siguiente;
+        }else{
+            auxAnterior->siguiente= aux->siguiente;
+        }
+
+        aux->siguiente = NULL;
+    }
+    return aux;
+}
+
+void insertarNodo(Lista *L, struct Nodo *nodoAInsertar){
      nodoAInsertar->siguiente = *L;
      *L = nodoAInsertar;
 
 }
 
 void mostrarTarea(Tarea t){
-    printf("id: %d - desc: %s", t.TareaID , t.Descripcion);
+    printf("id: %d - desc: %s", t.TareaID,t.Descripcion);
 }
 
 void mostrarLista(Lista L){
     while (L != NULL)
     {
-        mostrarTarea(*(L->T));
+        printf("id: %d - desc: %s - siguiente %p \n ", L->T.TareaID , L->T.Descripcion, L->siguiente);
         L = L->siguiente;
     }
     
